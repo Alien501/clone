7cfa508d57dce747e384413c3db4881320862f10
Alien501 <cvignesh404@gmail.com>
2025-10-27 21:38:12 +0530

    Done

--- grid-hover/source/src/App.tsx
+++ grid-hover/source/src/App.tsx
@@ -1,17 +1,53 @@
+import { useState } from 'react'
 import './App.css'
 import BoxHover from './BoxHover'
 
 function App() {
+  const [config, setConfig] = useState({
+    row: 10,
+    column: 10,
+    symbol: '+',
+    numberOfTrail: 10
+  });
+
+  const onValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setConfig(prev => {
+      return {
+        ...prev,
+        [id]: id === 'symbol' ? value : Number(value)
+      }
+    })
+  }
 
   return (
     <>
       <div className="box-container">
         <BoxHover
-          row={10}
-          column={10}
-          symbol='+'
-          numberOfTrail={10}
+          row={config.row}
+          column={config.column}
+          symbol={config.symbol}
+          numberOfTrail={config.numberOfTrail}
         />
+
+        <div className="controls">
+          <div className="control-group">
+            <label htmlFor="row">Rows: </label>
+            <input value={config.row} onChange={onValueChange} type="number" name="row" id="row" min="1" max="20" />
+          </div>
+          <div className="control-group">
+            <label htmlFor="column">Columns: </label>
+            <input value={config.column} onChange={onValueChange} type="number" name="column" id="column" min="1" max="20" />
+          </div>
+          <div className="control-group">
+            <label htmlFor="symbol">Symbol: </label>
+            <input value={config.symbol} onChange={onValueChange} type="text" name="symbol" id="symbol" maxLength={1} />
+          </div>
+          <div className="control-group">
+            <label htmlFor="numberOfTrail">Trail Length: </label>
+            <input value={config.numberOfTrail} onChange={onValueChange} type="number" name="numberOfTrail" id="numberOfTrail" min="1" max="50" />
+          </div>
+        </div>
       </div>
     </>
   )
